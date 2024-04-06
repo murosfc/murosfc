5932b3cddd0fd196465cadca03557eea4b1f9130
Felipe Muros <84643909+murosfc@users.noreply.github.com>
2024-04-06 20:27:14 -0300

    feat(site): added site naty

---
+++ SiteNaty/inserirmenu.js
@@ -0,0 +1,24 @@
+var stringmenu ='<header class="header"><span class="headerTitle" style="font-size: 1.5em">'+
+'Nathália Xavier Consultoria &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp</span>'+
+'<button class="menu-open">'+'<span class="material-icons headerButtonSpan">menu</span> </button>'+'<div class="overlay"></div>'+
+'<nav class="menu">'+'<a href="#" class="menu-close">&times;</a>'+'<ul>'+'<li><a href="index.html" target="_blank">Inicio</a></li>'+
+'<li><a href="analisedecor.html" target="_blank">Análise de coloração pessoal</a></li>'+'<li><a href="#" target="_blank">Consultoria completa</a></li>'+
+'<li><a href="consultoriaonline.html" target="_blank">Consultoria online</a></li>'+'<li><a href="revitarmario.html" target="_blank">Revitalização de armário</a></li>'+
+'<li><a href="personalshopper.html" target="_blank">Personal shopper</a></li>'+'<li><a href="packingwstyle.html" target="_blank">Packing with style</a></li>'+
+'</ul>'+'</nav>'+'</header>'
+document.write(stringmenu);
+
+
+
+
+    
+
+
+    
+
+          
+
+    
+   
+        
+
